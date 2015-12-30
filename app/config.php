a80242f0d5e691680d62bc0b05e3d183daa82247
Ali Dehghan <aliweb.ir@gmail.com>
2015-12-29 22:21:38 -0800

    Create config.php

---
+++ app/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('PA_MYSQL_HOSTNAME', "127.0.0.1");
+define('PA_MYSQL_USERNAME', "");
+define('PA_MYSQL_PASSWORD', "");
+define('PA_MYSQL_DATABASE', "");
+
+
+define('PA_APP_STATUS', 'developing'); // possible values: developing , deployed
+define('PA_UNDER_MAINTENANCE', 0); // if 1, site will not be displayed
